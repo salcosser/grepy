7b23d7113762e433c355bf7d7a699a102e2129b2
Samuel Alcosser <samalcosser@gmail.com>
2021-04-30 11:04:53 -0400

    setup project, and started processing

---
+++ _Grepy/grepy/.history/src/main/java/com/sgrep/app/Grepy_20210430110329.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Grepy{
+    public ArrayList<char> parseAlpha(String rawReg){
+        ArrayList<char> alphabet = new ArrayList<char>();
+
+        for(int i = 0;i<rawReg.length();i++){
+            System.out.println(rawReg.charAt(i));
+        }
+    }
+}
